a3733bee8e7d8fb329c838b357447097e44fc712
Felipe Oliveira <mec.foliveira@hotmail.com>
2021-01-01 23:56:28 -0300

    Regressive For task

---
+++ RegressiveFor.java
@@ -0,0 +1,8 @@
+public class RegressiveFor {
+    public static void main(String args[]) {
+        int num = 0;
+        for (int i = 9; i >= num; i--){
+            System.out.print(i+" ");
+        }
+    }
+}
