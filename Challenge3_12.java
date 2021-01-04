e7233c41d6ef79fc68197ee5e5166df3572ab7f5
Felipe Oliveira <mec.foliveira@hotmail.com>
2021-01-04 14:46:11 -0300

    challenge 3-12 solution

---
+++ Challenge3_12.java
@@ -0,0 +1,19 @@
+/*
+Write a program that reads a value and checks if it is less than 10. 
+Sample Input 1: 5
+Sample Output 1: true
+*/
+
+import java.util.Scanner;
+
+class Challenge3_12 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int num = scanner.nextInt();
+        
+        boolean result = num < 10;
+        
+        System.out.println(result);
+    }
+}
