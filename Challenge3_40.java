f7f7b66176757e3eb6b000b35bc4300671cf009a
Felipe Oliveira <mec.foliveira@hotmail.com>
2021-01-06 20:07:37 -0300

    Correction of identation in the Challenge3_40

--- Challenge3_40.java
+++ Challenge3_40.java
@@ -32,7 +32,7 @@ class Challenge3_40 {
         int number = scan.nextInt();
         System.out.print(number + " ");
         
-        while(number != 1) {
+        while (number != 1) {
             
             if (number % 2 == 0) {
                 number /= 2;
