3237325f323741fa42a9270483c4160b598be6b1
Felipe Oliveira <mec.foliveira@hotmail.com>
2021-01-10 22:50:19 -0300

    Resolution of Challenge4_03.java

---
+++ Challenge4_03.java
@@ -0,0 +1,17 @@
+/*
+Create an array of chars named characters with four elements 'a', 'z', 'e', 'd' and output it.
+
+Use the provided code template.
+*/
+
+import java.util.Arrays;
+
+public class Challenge4_03 {
+
+    public static void main(String[] args) {
+
+        char[] characters = {'a', 'z', 'e', 'd'};
+
+        System.out.println(Arrays.toString(characters));
+    }
+}
