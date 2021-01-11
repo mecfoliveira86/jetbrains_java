d8aa4fda8c10a1d7ee4129d072e736f7c37fdac6
Felipe Oliveira <mec.foliveira@hotmail.com>
2021-01-10 22:41:42 -0300

    Resolution of Challenge4_02.java

---
+++ Challenge4_02.java
@@ -0,0 +1,17 @@
+/*
+Initialize an int array named numbers with five elements 12, 17, 8, 101, 33 and output it.
+
+Use the provided code template.
+*/
+
+import java.util.Arrays;
+
+public class Challenge4_02 {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {12, 17, 8, 101, 33}; // initialize the array
+
+        System.out.println(Arrays.toString(numbers));
+    }
+}
