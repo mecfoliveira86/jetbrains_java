e5dc12a9f5f20a0c755fa94bf5bb437ec85e86df
Felipe Oliveira <mec.foliveira@hotmail.com>
2021-01-19 23:53:15 -0300

    Resoulution of Challenge4_14.java

--- Challenge4_14.java
+++ Challenge4_14.java
@@ -30,3 +30,39 @@ Sample Output 2:
 false
 */
 
+import java.util.*;
+
+class Challenge4_14 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int length = sc.nextInt();
+        int[] numbers = new int[length];
+        int[] search = new int[2];
+        
+        for (int i = 0; i < length; i++) {
+            numbers[i] = sc.nextInt();
+        }
+        
+        for (int j = 0; j < 2; j++) {
+            search[j] = sc.nextInt();
+        }
+        
+        int[] position = new int[2];
+        
+        for (int k = 0; k < length; k++) {
+            for (int l = 0; l < 2; l++) {
+                if (numbers[k] == search[l]) {
+                    position[l] = k;
+                }
+            }
+        }
+        
+        boolean result = false;
+        
+        if (Math.abs(position[0] - position[1]) == 1) {
+            result = true;
+        }
+        System.out.println(result);
+    }
+}
