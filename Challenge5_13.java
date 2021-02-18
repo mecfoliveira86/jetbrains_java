240d81e7cc00715b33f1c9da1d1b4a70f1f12a12
Felipe Oliveira <mec.foliveira@hotmail.com>
2021-02-18 17:25:17 -0300

    Resolution of Challenge5_132.java

---
+++ Challenge5_13.java
@@ -0,0 +1,23 @@
+/*
+Here's a program that may throw NullPointerException. Fix it to avoid the exception.
+
+In the case when the exception may occur, the program should print the string "NPE!".
+*/
+
+import java.util.*;
+
+class Challenge5_13 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String string = scanner.nextLine();
+        string = "null".equals(string) ? null : string;
+        /* Do not change code above */
+        if (string == null) {
+            System.out.println("NPE!");
+        } else {
+            System.out.println(string.toLowerCase());
+        }
+
+    }
+}
